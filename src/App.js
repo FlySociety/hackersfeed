3f8203df423bcb96db7f23b15621e98190cf5bd1
Patrik Rose <patrikrose88@gmail.com>
2017-03-31 15:45:55 -0700

    minor updates

--- src/App.js
+++ src/App.js
@@ -8,22 +8,25 @@ const list = [
 ];
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      { list.map(function(item) {
-        return (
-          <div> 
-            <span> 
-              <a href={item.url}>{item.title}</a> 
-            </span> 
-            <span> {item.author}</span>
-            <span> {item.num_comments}</span> 
-            <span> {item.points}</span> 
-          </div>
-      ); })}
-      </div>
-      );
+  constructor(props) { 
+    super(props);
+
+    this.state = { 
+      list: list, 
+    };
+  }
+
+  render() { 
+    return ( 
+      <div className="App"> 
+      { this.state.list.map(item =>
+        <div key={item.objectID}> 
+          <span> <a href={item.url}>{item.title}</a> 
+          </span> <span>{item.author}</span> 
+          <span>{item.num_comments}</span> 
+          <span>{item.points}</span> 
+        </div> )} 
+      </div> );
   }
 }
 
